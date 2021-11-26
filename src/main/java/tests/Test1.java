5f6e75c52f7ff4acfd1c8d4eec59ccfba502853e
wyf <zimo@example.com>
2021-11-26 11:07:49 +0800

    测试

--- src/main/java/tests/Test1.java
+++ src/main/java/tests/Test1.java
@@ -7,59 +7,20 @@ import com.alibaba.fastjson.JSONObject;
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.context.ApplicationContext;
+import other.Men;
 import tests.exception.RpcException;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.UUID;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Test1 {
 
     private final static Logger log = LoggerFactory.getLogger(Test1.class);
 
     public static void main(String[] args) throws RpcException {
-
-        JSONObject jsonObject = JSONObject.parseObject("{\n" +
-                "      \"area_ids\": [\n" +
-                "        {\n" +
-                "         \"area_id\":\"bab9e10b-ecf8-4446-9a0a-fdddca6be41f\"\n" +
-                "        },\n" +
-                "        {\n" +
-                "         \"area_id\":\"4fae926f-286a-4227-9cfc-c8d274e57e3f\"\n" +
-                "        },\n" +
-                "        {\n" +
-                "         \"area_id\":\"4fae926f-286a-4227-9cfc-c8d274e57e3f2\"\n" +
-                "        }\n" +
-                "      ]\n" +
-                "}");
-        log.info("请求报文为:{}",jsonObject);
-        Execution execution = new Execution();
-
-        //拿到areaIds
-        JSONArray areaIds = jsonObject.getJSONArray("area_ids");
-        log.info("areaIds is {}",areaIds.toString());
-
-        for(int i = 0 ; i < areaIds.size();i++){
-            JSONObject json = (JSONObject) areaIds.get(i);
-            String areaId = json.getString("area_id");
-            log.info("the {} areaId is {}",i,areaId);
-            if(StringUtils.isEmpty(areaId)){
-                throw new RpcException("参数校验异常，areaId不得为空");
-            }
-        }
-
-        JSONObject paramJson = new JSONObject();
-
-        String groupId = UUID.randomUUID().toString();
-
-        paramJson.put("groupId",groupId);
-        paramJson.put("areaIds",areaIds);
-
-        log.info("groupId is {}",groupId);
-        log.info("paramJson ids {}",paramJson);
-
-        execution.setVariable("paramJson",paramJson);
-
+        List<String> strings = Arrays.asList("123", "123", "222", "333");
+        int size = strings.stream().distinct().collect(Collectors.toList()).size();
+        System.out.println(size);
     }
 }
---
+++ src/main/java/tests/User.java
@@ -0,0 +1,35 @@
+package tests;
+
+
+public class User {
+
+    private String username;
+
+    private String sex;
+
+    private String email;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
